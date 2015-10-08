8b68bffc83280a939bbd543937d8f80b997caf92
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-10-08 13:37:30 +0200

    ADD: Register fragment

---
+++ app/src/main/java/ch/hsr/mge/gadgeothek/registerFragment.java
@@ -0,0 +1,14 @@
+package ch.hsr.mge.gadgeothek;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class registerFragment extends Fragment {
+
+}
