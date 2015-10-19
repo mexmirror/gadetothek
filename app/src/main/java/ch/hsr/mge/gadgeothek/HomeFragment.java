f9defd9eee76d09ef5b864f6cfb1c9049f0c82b5
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-10-19 11:38:57 +0200

    FIX: show support action bar

--- app/src/main/java/ch/hsr/mge/gadgeothek/HomeFragment.java
+++ app/src/main/java/ch/hsr/mge/gadgeothek/HomeFragment.java
@@ -11,14 +11,10 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         AppCompatActivity appCompatActivity = (AppCompatActivity)getActivity();
         appCompatActivity.getSupportActionBar().setTitle("Home");
         appCompatActivity.getSupportActionBar().show();
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
-
 }
